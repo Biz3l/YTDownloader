2cffe912899abd834241d72c6a495916dc8a0878
Biz3l <gabrieloliveiralveslegal@gmail.com>
2026-03-20 06:27:33 -0300

    fix vite for yt-dlp-exec

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         'electron-updater',
         'ffmpeg-static',
         'fluent-ffmpeg',
-        'yt-dlp-exec'
+        '@distube/ytdl-core'
       ]
     }
   },
